f5cb6bc374e3fe9f7156479a88f1f3bfca9ec6fc
Perrin Emmanuel Nzaou <perrinemmanuelnzaou@gmail.com>
2026-02-17 14:19:35 +0000

    Ajout des types

---
+++ client/src/types/index.ts
@@ -0,0 +1,44 @@
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+}
+
+export interface AuthResponse {
+  message: string;
+  token: string;
+  user: User;
+}
+
+export interface Image {
+  id: string;
+  url: string;
+  publicId: string;
+  projectId: string;
+  createdAt: string;
+}
+
+export interface Technology {
+  id: string;
+  name: string;
+  projectId: string;
+  createdAt: string;
+}
+
+export interface Project {
+  id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+  images: Image[];
+  technologies: Technology[];
+}
+
+export interface ProjectInput {
+  name: string;
+  description: string;
+  technologies: string[];
+  images?: FileList;
+}
